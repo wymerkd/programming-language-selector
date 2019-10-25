e4b9e322efa1a928e4466de23dad10bcbcd32e96
wymerkd <wymerkd@gmail.com>
2019-10-25 15:13:41 -0700

    addbranching to display possible survey results.

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,21 @@ $(document).ready(function() {
     var fifthResponse = $("input:radio[name=fifthResponse]:checked").val();
     console.log(firstResponse, secondResponse, thirdResponse, fourthResponse, fifthResponse)
 
-    if (firstResponse === 'mac') {
+
+    if (firstResponse === 'mac' && secondResponse === "iphone") {
       $('#language1').show();
+    }
+
+    if (firstResponse === 'mac' && secondResponse === "android") {
+    $('#language2').show();
+    }
+
+    if (firstResponse === 'pc' && secondResponse === "iphone") {
+    $('#language1').show();
+    }
+
+    if (firstResponse === 'pc' && secondResponse === "android") {
+    $('#language2').show();
     }
   });
 });
