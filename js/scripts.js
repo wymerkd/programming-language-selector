29c34382a6cb883980b0a57642377890a04afa4d
wymerkd <wymerkd@gmail.com>
2019-10-25 11:23:13 -0700

    add variables to gather user responses to survey questions, add console.log to test variables.

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
   $("form#questionnaire").submit(function(event) {
     event.preventDefault();
     var firstResponse = $("input:radio[name=firstResponse]:checked").val();
-    console.log(firstResponse)
+    var secondResponse = $("input:radio[name=secondResponse]:checked").val();
+    var thirdResponse = $("input:radio[name=thirdResponse]:checked").val();
+    var fourthResponse = $("input:radio[name=fourthResponse]:checked").val();
+    var fifthResponse = $("input:radio[name=fifthResponse]:checked").val();
+    console.log(firstResponse, secondResponse, thirdResponse, fourthResponse, fifthResponse)
   });
 });
